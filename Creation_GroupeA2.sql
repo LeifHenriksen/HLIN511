bbafcd86dc7a2eac6a4ca6a95e0994993a6e73c0
LeifHenriksen <44870274+LeifHenriksen@users.noreply.github.com>
2019-11-01 16:53:32 +0100

    Update Creation_GroupeA2.sql

--- Creation_GroupeA2.sql
+++ Creation_GroupeA2.sql
@@ -1,1 +1,12 @@
+/*CREATE DATABASE HLIN511;*/
 
+CREATE TABLE Evenement(
+Id_ev INT NOT NULL AUTO_INCREMENT,
+Localisation NVARCHAR(1000),
+Theme NVARCHAR(255),
+Date_ev DATE,
+Descriptif TEXT,
+Effectif_min INT,
+Effectif_max INT,
+PRIMARY KEY (Id_ev)
+);
