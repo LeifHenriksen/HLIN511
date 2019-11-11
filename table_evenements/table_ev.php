f6a66bc2ccfb9b6ce87c2607eaa605b4ac5c78b1
leifhenriksen <leifhenriksen88@gmail.com>
2019-11-11 17:35:12 +0000

    tables

---
+++ table_evenements/table_ev.php
@@ -0,0 +1,34 @@
+<?php 
+    include 'pdo.php';
+?>
+<html>
+<header>
+    <title>PDO</title>
+    <link rel="StyleSheet" href="exo10.css">
+</header>
+<body>
+<h1>PDO<h1>
+<?php 
+    $mon_pdo = new_pdo("localhost","HLIN511","omvadmin","openmediavault");
+    if($mon_pdo == null)
+    {
+        echo 'ERREUR! avec le PDO';
+        echo '<br>';
+    }
+    else
+    {
+	if(true)
+        {
+            
+            $sql = "SELECT NOM_EVENT, ADRESSE, THEME, DATE_EV, DESCRIPTIF FROM EVENEMENT";
+            $resultat = $mon_pdo->query($sql);
+            print_table("EVENEMENTS",$resultat); 
+        }
+        else
+        {
+            echo 'Erreur, manque de attributs <br>';
+        }
+    }
+?>
+</body>
+</html>
