423107becfff2a843c10f48d30cddee40b8b74a6
leif.henriksen-larez@etu.umontpellier.fr <leif.henriksen-larez@etu.umontpellier.fr@q11.fdsetu.infra.umontpellier.fr>
2019-11-15 11:48:55 +0100

    fdsfs

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) 
 {
     header('Location: login/principale.php');
-} 
+}   
 else 
 {
     header('Location: login/login.php');
