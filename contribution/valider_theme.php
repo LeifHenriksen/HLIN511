a9ce88c946aec7a6282928d56ab557369e0e1cde
leifhenriksen <leifhenriksen88@gmail.com>
2019-12-08 14:03:42 +0000

    NULL si le theme pere est vide

--- contribution/valider_theme.php
+++ contribution/valider_theme.php
@@ -1,17 +1,21 @@
 <?php
 session_start();
 include '../bdd_class/bdd_class.php';
-if (isset($_POST['theme_pere']) && isset($_POST['nom_theme']))
+if (isset($_POST['nom_theme']))
      {
         if(!empty($_SESSION['username']) && $_SESSION['user_type'] == 1)
         {
             $bdd = new DataBase();
-            $sql="
-            INSERT INTO THEME VALUES
-            ('".$_POST['nom_theme']."',
-             '".$_POST['theme_pere']."',
-             (SELECT THEME_RACINE FROM THEME T WHERE T.NOM_THEME LIKE '".$_POST['theme_pere']."'))";
-
+            if(!isset($_POST['theme_pere']))
+            	$sql="
+            	INSERT INTO THEME VALUES
+            	('".$_POST['nom_theme']."',
+             	'".$_POST['theme_pere']."',
+             	(SELECT THEME_RACINE FROM THEME T WHERE T.NOM_THEME LIKE '".$_POST['theme_pere']."'))";
+             else
+               $sql="
+            	INSERT INTO THEME VALUES
+            	('".$_POST['nom_theme']."', NULL, NULL)";
             try
             {
                 $bdd->getPDO()->query($sql);
